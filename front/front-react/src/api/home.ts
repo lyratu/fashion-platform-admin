82d85e42b7c7909608679b631c3d2d7d4d60ef84
akko <1421193732@qq.com>
2025-03-27 20:41:05 +0800

    del front end

--- front/front-react/src/api/home.ts
+++
@@ -1,8 +0,0 @@
-import request, { post, get } from '../utils/request'
-
-export const homeApi = {
-  // 获取主页信息
-  getHomeInfo() {
-    return post('/app/home/info/list', {})
-  }
-}
--- front/front-react/src/api/outfits.ts
+++
@@ -1,8 +0,0 @@
-import request, { post, get } from '../utils/request'
-
-export const outfitsApi = {
-  // 获取穿搭分享文章列表
-  getOutfitsList() {
-    return post('/app/outfits/info/page')
-  }
-}
